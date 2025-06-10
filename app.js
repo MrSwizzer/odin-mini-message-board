d035c26a5e05627306f3dadf525f0eff3596551e
MrSwizzer <155651796+MrSwizzer@users.noreply.github.com>
2025-06-10 11:47:08 +0200

    Initial Project Setup
    - Add indexRouter with routes for "/" and "/new"
    - Fix missing module.exports in indexRouter
    - Configure EJS as view engine and set views path
    - Add public directory for static assets
    - Render message list on index page using EJS
    - Create message partial for reusable layout
    - Pass message data correctly to the views

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const indexRouter = require('./routes/indexRouter');
+const path = require('node:path');
+
+const app = express();
+
+const assetsPath = path.join(__dirname, 'public');
+app.use(express.static(assetsPath));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
+
+const PORT = 5000;
+app.listen(PORT, () => {
+	console.log(`Express is listening on Port: ${PORT}`);
+});
---
+++ routes/indexRouter.js
@@ -0,0 +1,20 @@
+const { Router } = require('express');
+
+const messages = [
+	{
+		text: 'Hi there!',
+		user: 'Amando',
+		added: new Date(),
+	},
+	{
+		text: 'Hello World!',
+		user: 'Charles',
+		added: new Date(),
+	},
+];
+
+const indexRouter = Router();
+indexRouter.get('/', (req, res) => res.render('index', { title: 'Mini Messageboard', messages: messages }));
+indexRouter.get('/new', (req, res) => res.render('new'));
+
+module.exports = indexRouter;
